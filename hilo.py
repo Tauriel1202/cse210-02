5b10b4113fcd8f85a2db27dc5ecb1ece0301dd0e
drnodev <alexanortega@gmail.com>
2022-01-20 22:56:19 -0600

    hilo working basic

--- hilo.py
+++ hilo.py
@@ -1,35 +1,47 @@
-import random
 
-class Cards(): 
-  def __init__(self): 
-    self.value = 0 
-    self.points = 300 
+from deck import Deck
+
+"""
+TODO: Validations and doc
+"""  
+class hilo(): 
+
+
+  deck = None
+  points = 300
   
-  def card(self): 
-    self.cardFace = random.randint(1,13) 
-    self.newCard = random.randint(1,13) 
-    
-    if self.cardFace > self.newCard and self.user == 'h': 
-      self.points += 100 
-    elif self.cardFace < self.newCard and self.user == 'l':
-     self.points += 100 
-    else: self.points -= 75 
-  
-class game(): 
+
   def __init__(self): 
-    self.isPlaying = True 
-    self.points = 300 
-    self.cardFace = random.randint(1,13) 
-    def playing(self): 
-      while self.isPlaying: 
-        self.guess() 
-    
-    def guess(self): 
-      if self.points <= 0: 
-        self.isPlaying = False 
-      
-      print(self.cardFace) 
-      self.user = input('Is the next card higher or lower? (h/l) ').lower() 
-    
-Cards() 
-game().playing()
+    self.deck = Deck()
+
+
+  def play(self):
+
+    """ This method has the game logic """
+    while self.is_game_over() != True:
+
+      print(f"\nThe card is:{self.deck.last_card}")
+      option = input("Higher or Lower? [l/h]")
+      print(f"The next card was: {self.deck.next_card()}")
+
+      """ This block of code check if add points or not """
+      if (self.deck.current_card > self.deck.last_card ) and (option != 'l'):
+         self.points+= 100
+      elif (self.deck.current_card < self.deck.last_card ) and (option != 'h'): 
+          self.points+=100
+      else:
+          self.points -=75
+      """ end add points or not """
+
+      print(f"Your score is: {self.points}")
+      self.deck.shuffle() #updating the values
+      play = input(f"Play again? [y/N] ")
+      if(play != 'y'):
+        break
+
+  def is_game_over(self):
+    """This method check the points"""
+    if self.points <= 0:
+      return True
+    else: 
+      return False   
---
+++ main.py
@@ -0,0 +1,8 @@
+from hilo import hilo
+
+def main():
+ hilo().play()
+
+
+if __name__ == "__main__":
+    main()
